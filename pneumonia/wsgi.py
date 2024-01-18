444bc3f570a631543022f41550d7635e2755a63b
RUSOKE MARVIN <101735380+RusokeMarvin@users.noreply.github.com>
2024-01-18 16:28:29 +0300

    postg

--- pneumonia/wsgi.py
+++ pneumonia/wsgi.py
@@ -11,4 +11,3 @@ import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pneumonia.settings")
 application = get_wsgi_application()
-app = application
